75ba1fd1f0a91a45c99e6f9c813f50d4a1d6e348
towhid046 <towhidmorol46@gmail.com>
2024-04-09 19:03:46 +0600

    show card data dynamically

--- src/components/Estate/Estate.jsx
+++ src/components/Estate/Estate.jsx
@@ -4,42 +4,43 @@ import { IoCarSportSharp } from "react-icons/io5";
 import { FaBath } from "react-icons/fa";
 import { FaBed } from "react-icons/fa6";
 import { PiSquaresFourThin } from "react-icons/pi";
+import PropTypes from 'prop-types'
 
 const Estate = ({ estate }) => {
-  const { image, id } = estate;
+  const { image, id, estate_title, location, area, bedrooms, garages, bathrooms } = estate;
 
   return (
     <div className="bg-base-100 border rounded-xl">
       <figure className="">
-        <img className="rounded-t-xl" src={image} alt="Shoes" />
+        <img className="rounded-xl" src={image} alt="Shoes" />
       </figure>
       <div className="p-5 space-y-5">
         <div className="text-center">
           <h2 className="text-2xl font-semibold mb-3">
-            Sofi Berryessa 750 King Road
+            {estate_title}
           </h2>
-          <p className="flex items-center text-lg font-medium gap-2 mb-6 justify-center">
+          <p className="flex items-center text-md font-medium gap-2 mb-6 justify-center">
             <LuMapPin />
-            <span>Location your road </span>
+            <span>{location}</span>
           </p>
           <hr />
         </div>
         <ul className=" grid md:grid-cols-2 gap-4 grid-cols-1">
           <li className="flex items-center gap-2">
             <PiSquaresFourThin />
-            <span>23 Squer Feet</span>
+            <span>{area}</span>
           </li>
           <li className="flex items-center gap-2">
             <IoCarSportSharp />
-            <span>4 Garages</span>
+            <span>{garages} Garages</span>
           </li>
           <li className="flex items-center gap-2">
             <FaBed />
-            <span>Beedrooms</span>
+            <span>{bedrooms} Beedrooms</span>
           </li>
           <li className="flex items-center gap-2">
             <FaBath />
-            <span>4 Bathrooms</span>
+            <span>{bathrooms} Bathrooms</span>
           </li>
         </ul>
 
@@ -56,5 +57,9 @@ const Estate = ({ estate }) => {
     </div>
   );
 };
+
+Estate.propTypes = {
+    estate: PropTypes.object.isRequired
+}
 
 export default Estate;
